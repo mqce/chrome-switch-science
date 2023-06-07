d20151f303257ca46c25331ada8cf919cf956e78
hisashi nakagawa <0x0muu@gmail.com>
2023-06-07 13:10:43 +0900

    Update embed.js

--- src/embed.js
+++ src/embed.js
@@ -1,20 +1,21 @@
 // content.jsからはwindowオブジェクトが取得できないのでembedから送る
 // 参考: https://zenn.dev/ellreka/articles/799632c02d1cb5
+
 window.addEventListener('message', (event) => {
   if(
     event.source == window && 
     event?.data?.type === 'FROM_CONTENT'
   ){
-    const data = JSON.stringify(window.__pageflyProducts);
-    window.postMessage(
-      { type: 'FROM_EMBED', data },
-      'https://www.switch-science.com'
-    );
+    postPageflyData();
   }
 }, false);
 
-const data = JSON.stringify(window.__pageflyProducts);
-window.postMessage(
-  { type: 'FROM_EMBED', data },
-  'https://www.switch-science.com'
-);
+postPageflyData();
+
+function postPageflyData(){
+  const data = JSON.stringify(window.__pageflyProducts);
+  return window.postMessage(
+    { type: 'FROM_EMBED', data },
+    'https://www.switch-science.com'
+  );
+}
